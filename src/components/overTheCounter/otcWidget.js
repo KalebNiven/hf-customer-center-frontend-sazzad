059bdc6903186cfe2d9a9dc9c04f8f9005619d3c
sshivraj <sshivraj@healthfirst.org>
2023-08-18 13:23:56 -0400

    Event handling

--- src/components/overTheCounter/otcWidget.js
+++ src/components/overTheCounter/otcWidget.js
@@ -1,22 +1,85 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import styled from "styled-components";
+import { useAppContext } from "../../AppContext";
+import { ANALYTICS_TRACK_CATEGORY } from "../../constants/segment";
 import { loadExternalScript } from "../../utils/externalScripts";
 
 const OTC_WIDGET_SCRIPT_ID = 'OTCWidgetScript';
+
+const SSO = 'sso';
+const otcWidgetLinks = {
+  navigateToBenefitsCenter: {
+    type: SSO,
+    link: process.env.MIX_REACT_APP_OTC_NETWORK_HREF_V2
+  },
+  navigateToMomsMeals: {
+    type: null,
+    link: "https://www.grownyc.org/greenmarket/ourmarkets"
+  },
+  navigateToNationsOTC: {
+    type: SSO,
+    link: process.env.MIX_REACT_APP_NATIONS_OTC_HREF_V2
+  },
+  navigateToOTCNetwork: {
+    type: SSO,
+    link: process.env.MIX_REACT_APP_OTC_NETWORK_HREF_V2
+  },
+  navigateToGrowNYC: {
+    type: null,
+    link: "https://www.grownyc.org/greenmarket/ourmarkets"
+  },
+  navigateToNationsBenefits: {
+    type: SSO,
+    link: process.env.MIX_REACT_APP_NATIONS_OTC_HREF_V2
+  }
+}
 const OTCWidget = () => {
 
   const { MIX_REACT_TRAILBLAZER_WIDGET_BASE_URL } = process.env;
   const customerInfo = useSelector((state) => state.customerInfo.data);
+  const memberId = customerInfo?.hohPlans[0]?.MemberId;
   const jwt_token = customerInfo.id_token
   const updatedJwt = (jwt_token === undefined ? jwt_token : jwt_token.replace('Bearer ', ''));
   const [existingScript, setExistingScript] = useState(document.getElementById(OTC_WIDGET_SCRIPT_ID));
+  const { externalSiteModal, setExternalSiteModal } = useAppContext()
 
-  const events = {};
+  const handleExternalSiteClicked = (link, type, action) => {
+    let externalSiteData = {
+      ...externalSiteModal, 
+      isVisible: true, 
+      link: link, 
+      target: "_blank", 
+      segmentPageCategory: ANALYTICS_TRACK_CATEGORY.otc, 
+      segmentTargetMemberId: customerInfo.memberId, 
+      label: action,
+    };
+    if(type === SSO) externalSiteData["membershipKey"] = customerInfo.hohPlans[0].MembershipKey;
+
+    setExternalSiteModal(externalSiteData);
+  };
+
+  const events = {
+    onOTCInfoLinkClicked: (data) => {
+      const{ identifier, type, url, action } = data;
+      switch(action){
+        case 'navigateToBenefitsCenter':
+        case 'navigateToMomsMeals':
+        case 'navigateToNationsOTC':  
+        case 'navigateToOTCNetwork': 
+        case 'navigateToGrowNYC': 
+        case 'navigateToNationsBenefits': 
+          handleExternalSiteClicked(otcWidgetLinks[action].link, otcWidgetLinks[action].type, action);
+          break;
+        default: 
+          return;
+      }
+    }
+  }
 
   const mountProps = {
       parentElement: '#otcw-main',
-      memberId:customerInfo.memberId,
+      memberId:memberId,
       appId: 'CUSTOMER_CENTER',
       authorizer: 'OKTA',
       lang: customerInfo.language,
