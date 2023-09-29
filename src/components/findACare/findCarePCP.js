49fd5100a8fbb35e0fcefdbff6884f42f6e4759c
sshivraj <sshivraj@healthfirst.org>
2023-09-29 14:55:32 -0400

    correct the boolean

--- src/components/findACare/findCarePCP.js
+++ src/components/findACare/findCarePCP.js
@@ -75,8 +75,8 @@ const FindCarePCP = (props) => {
           }) || [];
 
         const memberDetails = [
-            ...hohPlans.filter(plan => plan.disablePcpUpdate === true),
-            ...memberDependents.filter(plan => plan.disablePcpUpdate === true)
+            ...hohPlans.filter(plan => plan.disablePcpUpdate === false),
+            ...memberDependents.filter(plan => plan.disablePcpUpdate === false)
         ];
     
             let mountProps 
