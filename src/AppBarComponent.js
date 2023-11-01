add8b2082c3fdc781f139bb18470ea366c6a8064
sshivraj <sshivraj@healthfirst.org>
2023-11-01 14:03:54 -0400

    empty string if last name not present

--- src/AppBarComponent.js
+++ src/AppBarComponent.js
@@ -78,7 +78,7 @@ function AppBarComponent() {
   const classes = useStyles(mobileDrawerTop)();
   const customerInfo = useSelector((state) => state.customerInfo);
   const [openUserCard, setOpenUserCard] = useState(false);
-  const userName = (`${customerInfo.data.firstName} ${customerInfo.data.lastName}`);
+  const userName = (`${customerInfo.data.firstName} ${customerInfo.data.lastName ? customerInfo.data.lastName : ""}`);
   const { firstName } = customerInfo.data;
   const [homeMobileItems, setHomeMobileItems] = useState(false);
   const [findCareMobileItems, setFindCareMobileItems] = useState(false);
