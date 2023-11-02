ef8938fb917c2dc2161fe0a14b96410c87dd6849
Piatkov, Igor <ipiatkov@Healthfirst.org>
2023-11-02 15:35:06 -0400

    redirect to home fix

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,20 +1,23 @@
 import { useOktaAuth } from '@okta/okta-react';
 import { useDispatch } from "react-redux";
 import { requestResetState } from '../store/actions';
+import { useHistory } from "react-router-dom";
 
 export const useLogout = () => {
   const { oktaAuth, authState } = useOktaAuth();
   const dispatch = useDispatch();
+  const history = useHistory();
 
   return async function() {
     analytics.reset();
     try {
+      history.push('/login')
+      sessionStorage.removeItem('from');
       await oktaAuth.signOut({ postLogoutRedirectUri: window.location.origin + '/login' })
       oktaAuth.tokenManager.clear();
       localStorage.removeItem("identifySegmentFlag");
       sessionStorage.removeItem("currentMemberId");
       sessionStorage.removeItem('SessionTimeStamp');
-      sessionStorage.removeItem('from');
       sessionStorage.removeItem(`persist:${window.location.host}_PROVIDER_APP`);
       sessionStorage.removeItem(`persist:${process.env.MIX_REACT_PAYMENTS_BASE_URL}`);
       sessionStorage.removeItem('skipAddMembership');
