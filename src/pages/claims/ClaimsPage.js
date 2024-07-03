699a5655b60810288c36d244a6df853691e66050
sshivraj <sshivraj@healthfirst.org>
2024-07-03 13:08:36 -0400

    integrate claims widget.

--- src/pages/claims/ClaimsPage.js
+++ src/pages/claims/ClaimsPage.js
@@ -1,12 +1,26 @@
+import { useClient } from "@splitsoftware/splitio-react";
 import React from "react";
 import ClaimsPage from "../../components/claims/claimsPage";
+import ClaimsWidget from "../../components/claims/ClaimsWidget";
 import { CLAIMS_PAGE } from "../../constants/splits";
 import PageLayout from "../../layouts/PageLayout";
 
 export default () => {
-  return (
-    <PageLayout splitFeatureName={CLAIMS_PAGE}>
-      <ClaimsPage />
-    </PageLayout>
-  );
+  const splitHookClient = useClient();
+  const claimsPageSplitTreatment =
+    splitHookClient.getTreatment(CLAIMS_PAGE);
+
+    console.log({claimsPageSplitTreatment})
+  switch (claimsPageSplitTreatment) {
+    case "on":
+      return <ClaimsWidget />;
+    case "legacy":
+      return <ClaimsPage />;
+    case "off":
+      return <PermissionDenied />;
+    case "control":
+      return <></>;
+    default:
+      return <></>;
+  }
 };
