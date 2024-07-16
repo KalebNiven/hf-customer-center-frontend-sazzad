d579bb774c9252d36b023474dc46e63d82464678
sshivraj <sshivraj@healthfirst.org>
2024-07-16 09:22:30 -0400

    URL

--- src/components/claims/ClaimsWidget.js
+++ src/components/claims/ClaimsWidget.js
@@ -90,7 +90,7 @@ function ClaimsWidget() {
       }
     } else {
       loadExternalScript(
-        MIX_REACT_CC_WIDGETS_BASE_URL + "/cc-claims-widget.js",
+        MIX_REACT_CC_WIDGETS_BASE_URL + "/claims/cc-claims-widget.js",
         CLAIMS_WIDGET_SCRIPT_ID,
         () => {
           try {
