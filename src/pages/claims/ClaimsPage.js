8531a0a10311a746a3ed89d4b1a00bd39d8ccd08
sshivraj <sshivraj@healthfirst.org>
2024-07-29 09:40:26 -0400

    correct the import

--- src/pages/claims/ClaimsPage.js
+++ src/pages/claims/ClaimsPage.js
@@ -1,5 +1,6 @@
 import { useClient } from "@splitsoftware/splitio-react";
 import React from "react";
+import PermissionDenied from "../../components/common/PermissionDenied";
 import ClaimsPage from "../../components/claims/claimsPage";
 import ClaimsWidget from "../../components/claims/ClaimsWidget";
 import { CLAIMS_PAGE } from "../../constants/splits";
