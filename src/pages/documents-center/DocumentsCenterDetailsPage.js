e84b1fc84a782c1416f32ed283755d69466f0675
Bira Mallick <bmallick@healthfirst.org>
2024-09-11 09:25:13 -0700

    update

--- src/pages/documents-center/DocumentsCenterDetailsPage.js
+++ src/pages/documents-center/DocumentsCenterDetailsPage.js
@@ -2,16 +2,10 @@ import React from "react";
 import GetDocument from "../../components/documents/getDocument";
 import DocumentCenterWidget from "../../components/documents/DocumentCenterWidget";
 import { DOCUMENTS_PAGE } from "../../constants/splits";
-import PageLayout from "../../layouts/PageLayout";
 import { useClient } from "@splitsoftware/splitio-react";
 import PermissionDenied from "../../components/common/PermissionDenied";
 
 export default () => {
-  // return (
-  //   <PageLayout splitFeatureName={DOCUMENTS_PAGE}>
-  //     <GetDocument />
-  //   </PageLayout>
-  // );
   const splitHookClient = useClient();
   const documentPageSplitTreatment =
     splitHookClient.getTreatment(DOCUMENTS_PAGE);
